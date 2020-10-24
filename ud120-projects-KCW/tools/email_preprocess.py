e7d547ecb38f4ad483aed179b6ee371b6f37c662
weikhor <cheanwei.KCW@gmail.com>
2020-10-25 00:23:35 +0800

    update sklearn

--- ud120-projects-KCW/tools/email_preprocess.py
+++ ud120-projects-KCW/tools/email_preprocess.py
@@ -5,6 +5,7 @@ import cPickle
 import numpy
 
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -39,7 +40,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validate.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
