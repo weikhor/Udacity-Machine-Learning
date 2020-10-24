f9dce6bd4c7cbaaef6b8b94186e09715f504c1c9
weikhor <cheanwei.KCW@gmail.com>
2020-10-25 00:16:56 +0800

    change to python 2

--- ud120-projects-KCW/tools/email_preprocess.py
+++ ud120-projects-KCW/tools/email_preprocess.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import pickle
+import cPickle
 import numpy
 
-from sklearn.model_selection import cross_validate
+from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -28,17 +29,17 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "rb")
+    authors_file_handler = open(authors_file, "r")
     authors = pickle.load(authors_file_handler)
     authors_file_handler.close()
 
-    words_file_handler = open(words_file, "rb")
+    words_file_handler = open(words_file, "r")
     word_data = cPickle.load(words_file_handler)
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validate.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
