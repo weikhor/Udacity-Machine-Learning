a36459778440a8a7ac7f05899f1eb19d8e0279b9
weikhor <cheanwei.KCW@gmail.com>
2020-10-25 00:06:12 +0800

    update

--- ud120-projects-KCW/tools/email_preprocess.py
+++ ud120-projects-KCW/tools/email_preprocess.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
 import pickle
-#import cPickle
 import numpy
 
-from sklearn import cross_validation
+from sklearn.model_selection import cross_validate
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
@@ -34,12 +33,12 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     authors_file_handler.close()
 
     words_file_handler = open(words_file, "r")
-    word_data = pickle.load(words_file_handler)
+    word_data = cPickle.load(words_file_handler)
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validate.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
