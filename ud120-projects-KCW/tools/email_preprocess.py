c5b2b468984c0783fff0338ca28ae7ac94f8a36a
weikhor <cheanwei.KCW@gmail.com>
2020-10-25 00:08:09 +0800

    change to byte

--- ud120-projects-KCW/tools/email_preprocess.py
+++ ud120-projects-KCW/tools/email_preprocess.py
@@ -28,11 +28,11 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "r")
+    authors_file_handler = open(authors_file, "rb")
     authors = pickle.load(authors_file_handler)
     authors_file_handler.close()
 
-    words_file_handler = open(words_file, "r")
+    words_file_handler = open(words_file, "rb")
     word_data = cPickle.load(words_file_handler)
     words_file_handler.close()
 
